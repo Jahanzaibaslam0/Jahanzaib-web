298f1b7d4792c71100ecf5da2412869bc315f930
Muhammad Jahanzaib <jahanzaibaslam0@gmail.com>
2026-06-08 20:47:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function renderLayout() {
       <div class="container footer-grid">
         <section class="footer-block">
           <h3>Get In Touch</h3>
-          <p>&#128222; &nbsp;+92 (21) 6823594</p>
+          <p>&#128222; &nbsp;+92 310 2087472</p>
           <p>&#9993; &nbsp;info@zilhajjcattlefarm.com</p>
           <p>&#128205; &nbsp;Main Super Highway M9, Karachi.</p>
         </section>
@@ -142,7 +142,7 @@ function renderLayout() {
             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">&#128337; Facebook</a>
             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">&#127774; Instagram</a>
             <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">&#9654; YouTube</a>
-            <a href="https://wa.me/92216823594" target="_blank" rel="noopener noreferrer">&#128172; WhatsApp</a>
+            <a href="https://wa.me/923102087472" target="_blank" rel="noopener noreferrer">&#128172; WhatsApp</a>
           </div>
         </section>
         <section class="footer-block">
